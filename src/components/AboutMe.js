267d6bbcbdbb3aa6d6ec27efeb44c0736e7bf0f1
Lana McKay <citlmu@MAC7YNLVDC-2.local>
2023-04-22 20:14:33 -0400

    added about me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,37 @@
+import React from "react";
+import FadeIn from "react-fade-in/lib/FadeIn";
+import lana from "../images/lanamckay.png";
+
+import "../css/AboutMe.scss";
+import "../css/Main.scss";
+
+const About = () => {
+  return (
+    <div className="section" id="about">
+      <div className="container">
+        <div className="about-section">
+          <div className="content">
+            <FadeIn bottom>
+              <h1> About Me </h1>
+            </FadeIn>
+            <p>
+              I'm a 3rd year student at the University of Waterloo studying Systems Design Engineering 💻
+              <br></br>
+              <br></br>
+              Through personal projects, hackathons, and previous work experience at Empire Life, Manulife I have gained valuable experience working in
+              software development and product management.
+              <br></br>
+              <br></br>
+              During my free time you can find me at the gym, travelling, eating good foods, or hanging out with my dog! {" "}
+            </p>
+          </div>
+          <div className="image-wrapper">
+            <img src={lana} alt="about"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
